2ae201b9d31ba4ab773a4d5b82d1918828d7c341
Andrei Santos <andrei.as3@hotmail.com>
2021-10-14 21:18:54 -0300

    coverage e2e conclued

---
+++ javascript-test/04-coverage-e2e/src/api.js
@@ -0,0 +1,49 @@
+const http = require('http');
+
+const DEFAULT_USER = { username: 'AndreiSantos', password: '123' };
+
+const routes = {
+  '/contact:get': (request, response) => {
+    response.write('contact us page');
+    return response.end();
+  },
+
+  '/login:post': async (request, response) => {
+    // response é um iterador!
+    for await (const data of request){
+      const user = JSON.parse(data);
+      //console.log('user', user);
+
+      if(user.username !== DEFAULT_USER.username && user.password !== DEFAULT_USER.password){
+        response.writeHead(401)
+        response.write('Logging failed!');
+        return response.end();
+      }
+
+      response.write('Logging has succeeded!');
+      return response.end();
+    }
+  },
+
+
+  default: (request, response) => {
+    response.write('Hello World');
+    return response.end()
+  }
+}
+
+const handler = function (request, response) {
+  const { url, method } = request;
+  const routerKey = `${url}:${method.toLowerCase()}`;
+  // console.log('routerKey', routerKey);
+  const chosen = routes[routerKey] || routes.default;
+  response.writeHead(200, {
+    'Content-Type': 'text/html'
+  });
+  return chosen(request, response);
+}
+
+const app = http.createServer(handler)
+                .listen(3000, () => console.log('app running at', 3000));
+
+module.exports = app;
