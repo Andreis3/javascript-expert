627ffcac749c59939c4374dedc2a323f38216bbf
Andreis3 <andrei.as3@hotmail.com>
2021-12-28 07:17:54 -0300

    completed module

---
+++ 03-conceitos-fundamentais-part2/03-esmodules-internacionalization-part2/src/index.js
@@ -0,0 +1,32 @@
+import TerminalController from "./TerminalController.js";
+import database from "../database.json";
+import Person from "./person.js";
+
+const DEFAULT_LANG = "pt-br";
+const STOP_TERMINAL = ":q";
+
+const terminalController = new TerminalController();
+terminalController.initializeTerminal(database, DEFAULT_LANG);
+
+async function mainLoop() {
+  try {
+    const answer = await terminalController.question("What??");
+    console.log("answer", answer);
+
+    if (answer === STOP_TERMINAL) {
+      terminalController.closeTerminal();
+      console.log("process finished");
+      return;
+    }
+
+    const person = Person.generateInstanceFromString(answer);
+    console.log("person", person.formatted(DEFAULT_LANG));
+
+    return mainLoop();
+  } catch (error) {
+    console.error("DEU RUIM**", error);
+    return mainLoop();
+  }
+}
+
+await mainLoop();
