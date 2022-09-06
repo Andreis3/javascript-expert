0c3b9080e632f5174bffcbc96a7de94be1d98a2b
andreis3 <andrei.as3@hotmail.com>
2022-09-06 08:43:14 -0300

    npm register

---
+++ javascript-expert/08-npm-register/01-my-first-npm-module/myapp/index.js
@@ -0,0 +1,17 @@
+// para importar do diretório use o comando abaixo
+// node --experimental-specifier-resolution=node index.js
+
+//import FluentSQLBuilder from './../fluentsql-jest-tdd-yt';
+import FluentSQLBuilder from "@andreis3/fluentsql";
+
+import database from './database/data.json' assert { type: 'json'};
+
+const result = FluentSQLBuilder.for(database)
+    .where({ registered: /^(2020|2019)/ })
+    .select(['name', 'category'])
+    .limit(3)
+    // .groupCount('category')
+    .countBy('category')
+    .build();
+
+console.log({ result });
