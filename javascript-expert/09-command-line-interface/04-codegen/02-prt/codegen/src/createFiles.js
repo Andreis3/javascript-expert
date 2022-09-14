d6edca119097c3108ed705e9155e6cb2a946e24a
andreis3 <andrei.as3@hotmail.com>
2022-09-14 08:45:40 -0300

    codegen prt 02

---
+++ javascript-expert/09-command-line-interface/04-codegen/02-prt/codegen/src/createFiles.js
@@ -0,0 +1,8 @@
+import fsPromises from 'fs/promises';
+import fs from 'fs';
+
+import templates from './templates/index.js';
+
+export async function createFiles({ mainPath, defaultMainFolder, layers, componentName }){
+
+}
